c4186440c4fb665f646e4d634b69adbbbdd119c2
LeinadOdnan <leinad.odnan@gmail.com>
2026-03-05 18:01:09 -0500

    1st instrument

---
+++ kick.rb
@@ -0,0 +1,9 @@
+#use_bpm 90
+#Only instruments, There wont be a structure, only loops and sync.
+# Relax Kick
+live_loop :kick do
+    sample :bd_sone, amp: 1.3
+    sleep 1.5
+    sample :bd_sone, amp: 1.1
+    sleep 0.5
+  end
